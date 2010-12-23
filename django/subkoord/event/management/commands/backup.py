cb4d6950eceabc9a74ec7f18fca560aff911fa60
Rupert Angermeier <rupert.angermeier@gmail.com>
2010-12-23 18:41:44 +0000

    event_reminder und backup nicht mehr über HTTP sondernniur noch als Befehle für manage.py ausführbar

---
+++ django/subkoord/event/management/commands/backup.py
@@ -0,0 +1,27 @@
+from django.template import RequestContext, loader
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.core.mail import send_mail
+from datetime import datetime, timedelta
+from django.utils.translation import ugettext as _
+from django.conf import settings
+from django.core.management.base import NoArgsCommand
+import os
+import subprocess
+
+class Command(NoArgsCommand):
+	help = 'Generate a dump from the database'
+	def handle_noargs(self, **options):
+		last_backup = datetime.fromtimestamp(os.path.getmtime(settings.BACKUP_DIR+"/backup.sql"))
+		if last_backup > datetime.now() - timedelta(hours=1):
+			return "only one backup per hour"
+		if settings.DATABASES['default']['HOST'] != "":
+			host = "-h " + settings.DATABASES['default']['HOST']
+		else: host = ""
+		user =  "-u " + settings.DATABASES['default']['USER']
+		password = "-p" + settings.DATABASES['default']['PASSWORD']
+		dbname = settings.DATABASES['default']['NAME']
+		mysqldump = "mysqldump --add-drop-table " + " ".join([user, password, host, dbname]) + " >  backup.sql"
+		subprocess.call(args=mysqldump, shell=True, cwd=settings.BACKUP_DIR)
+		subprocess.Popen(args="gzip -c backup.sql > backup.gz", shell=True, cwd=settings.BACKUP_DIR)
+		return "ceated DB backup"
---
+++ django/subkoord/event/management/commands/event_reminder.py
@@ -0,0 +1,36 @@
+from django.core.management.base import NoArgsCommand
+from django.template import loader
+from django.core.mail import send_mail
+from django.conf import settings
+from datetime import datetime, timedelta
+from event.models import Event
+
+class Command(NoArgsCommand):
+	help = 'Check for open tasks and send e-mail if necessary'
+	def handle_noargs(self, **options):
+		remind_window = timedelta(hours=settings.EVENT_REMINDER_WINDOW)
+		remind_pause = timedelta(hours=settings.EVENT_REMINDER_WINDOW)
+		now = datetime.now()
+		events = Event.objects.select_related(
+			).filter(date__gte = now
+			).filter(date__lte = now + remind_window + remind_pause
+			).exclude(cron__gte = now - remind_pause)
+		events_in_remind_window = False
+		for event in events:
+			if event.date <= now + remind_window:
+				events_in_remind_window = True
+			if event.all_tasks_satisfied:
+				events = events.exclude(id=event.id)
+		if events.count() > 0 and events_in_remind_window:
+			#addressbook = [u.email for u in User.objects.all()]
+			addressbook = settings.EVENT_REMINDER_ADDRESSBOOK
+			body = loader.render_to_string("email/open_tasks.html",
+				{'events': events, 'site_url': settings.SITE_URL,})
+			send_mail( settings.EVENT_REMINDER_SUBJECT, body,
+				settings.EVENT_REMINDER_FROM, addressbook, )
+			for event in events:
+			  event.cron = datetime.now()
+			  event.save()
+			return body
+		else:
+			return "no open tasks"
