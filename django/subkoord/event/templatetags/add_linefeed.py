77acb6676a2f791e1d02bf0dbaf01ef9a11b7702
Rupert Angermeier <rupert.angermeier@gmail.com>
2011-03-18 16:17:13 +0000

    Die Reminder e-mails informativer machen. Anmerkungen und Info wird nun mitgeschickt

---
+++ django/subkoord/event/templatetags/add_linefeed.py
@@ -0,0 +1,12 @@
+from django import template
+from django.utils.safestring import mark_safe
+import re
+
+def add_linefeed_filter(value, num=1):
+	f = '\t'*num
+	p = re.compile("\n")
+	value = p.sub(r'\n'+f, value)
+	return mark_safe(value)
+
+register = template.Library()
+register.filter('add_linefeed', add_linefeed_filter)
