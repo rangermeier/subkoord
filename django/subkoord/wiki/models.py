755382e163fdafe124c886bba04eeeb541fd2a8b
Rupert Angermeier <rupert.angermeier@gmail.com>
2012-02-22 20:57:59 +0100

    port auf 1.4: nicht definierte felder "name" aus model entfernt

--- django/subkoord/wiki/models.py
+++ django/subkoord/wiki/models.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.utils.translation import ugettext as _
 from django.contrib.contenttypes import generic
-from attachment.models import Attachment
+from subkoord.attachment.models import Attachment
 
 class Wikicategory(models.Model):
     """Category for pages"""
@@ -18,14 +18,14 @@ class Wikicategory(models.Model):
 
 class Wikipage(models.Model):
     """Wiki page storage"""
-    title = models.SlugField(max_length=50, unique = True, name=_("Title"), verbose_name=_("Page Title"),
+    title = models.SlugField(max_length=50, unique = True, verbose_name=_("Page Title"),
         help_text=_("Only letters, numbers, underscores and hyphens."))
-    content = models.TextField(name=_("Content"), verbose_name=_("Content"),
+    content = models.TextField(verbose_name=_("Content"),
         help_text=_("You can use Textile to format your text.")+"<br />"+_("Wiki-Link: <code>[[link_title]]</code>"))
-    content_html = models.TextField(name=_("Content"), editable=False)
+    content_html = models.TextField( editable=False)
     last_changed = models.DateTimeField(auto_now=True, editable=False)
     author = models.ForeignKey(User, editable=False)
-    category = models.ForeignKey(Wikicategory, null=True, name=_("Category"), verbose_name=_("Category"))
+    category = models.ForeignKey(Wikicategory, null=True, verbose_name=_("Category"))
     attachments = generic.GenericRelation(Attachment)
 
     def __unicode__(self):
