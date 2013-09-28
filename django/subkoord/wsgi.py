b42be15876f37e841b4f4bba105e5f162fdaa5c6
Rupert Angermeier <rupert.angermeier@gmail.com>
2013-09-28 12:14:18 +0200

    init newrelic agent if config is found
    config expected at django/subkoord/newrelic.ini

--- django/subkoord/wsgi.py
+++ django/subkoord/wsgi.py
@@ -1,5 +1,15 @@
 import os
 
+NEWRELIC_CONF = os.path.join(os.path.dirname(__file__), "newrelic.ini")
+if os.path.exists(NEWRELIC_CONF):
+    try:
+        import newrelic.agent
+        newrelic.agent.initialize(NEWRELIC_CONF)
+        print "starting with newrelic"
+    except ImportError:
+        pass
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subkoord.settings")
 
 # This application object is used by the development server
